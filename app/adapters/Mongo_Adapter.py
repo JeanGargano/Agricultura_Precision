df9ea30b7a8d1eadc56e6b265013e79861e528a3
JeanGargano <159099449+JeanGargano@users.noreply.github.com>
2024-11-06 16:04:14 -0500

    Trabajar con esta version

--- app/adapters/Mongo_Adapter.py
+++ app/adapters/Mongo_Adapter.py
@@ -29,10 +29,7 @@ class MongoAdapter(MongoInterface):
         try:
             micro_data = data.dict(by_alias=True)
             result = self.collection.insert_one(micro_data)
-            if result:
-                return "Se han guardado los datos correctamente"
-            else:
-                return "No se pudieron guardar los datos, verifique los tipos"
+            return result
         except PyMongoError as e:
             raise Exception(f"Fallo la conexion a la BD: {str(e)}")
 
